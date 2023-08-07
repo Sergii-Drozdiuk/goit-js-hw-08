f4219f0f66a71b6045ce131b7d5f6c87f90c16f0
Sergii-Drozdiuk <drozduyk@gmail.com>
2023-08-08 00:19:38 +0300

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,5 @@
 import SimpleLightbox from "simplelightbox";
-import "/node_modules/simplelightbox/dist/simple-lightbox.min.css";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
