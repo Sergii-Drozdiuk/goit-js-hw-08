f133ea0594af4fe6bfb0dd49e1fe8a46ea16d9d9
Sergii-Drozdiuk <drozduyk@gmail.com>
2023-08-11 17:51:51 +0300

    update-02-video

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,11 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+const player = new Player('vimeo-player');
+let time = JSON.parse(localStorage.getItem("videoplayer-current-time")) ?? 0;
 
+player.setCurrentTime(time);
+player.on('timeupdate', throttle(saveTime, 1000));
+
+function saveTime(data) {
+   localStorage.setItem("videoplayer-current-time", JSON.stringify(data.seconds));
+};
